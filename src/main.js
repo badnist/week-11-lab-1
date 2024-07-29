0cdbea4ccd039d15071786f51bbc79c2f6dce66c
badnist <ryan.marzban@outlook.com>
2024-07-29 20:21:27 +0200

    Done!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
+import router from "./router"
+import "bootstrap/dist/css/bootstrap.css"
+
+createApp(App).use(createPinia()).use(router).mount("#app")
